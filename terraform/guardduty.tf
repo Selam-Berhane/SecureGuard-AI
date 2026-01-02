ea90fb87f1f547a25b3f5e3dff57fa71858d03df
Selam Berhane <selamberhane@Selams-MacBook-Pro.local>
2026-01-02 11:26:06 -0500

    updated depreciated resource since last update

--- terraform/guardduty.tf
+++ terraform/guardduty.tf
@@ -4,30 +4,27 @@ resource "aws_guardduty_detector" "main" {
   enable                       = true
   finding_publishing_frequency = "FIFTEEN_MINUTES"
 
-  # Enable S3 protection
-  datasources {
-    s3_logs {
-      enable = true
-    }
-
-    kubernetes {
-      audit_logs {
-        enable = true
-      }
-    }
-
-    malware_protection {
-      scan_ec2_instance_with_findings {
-        ebs_volumes {
-          enable = true
-        }
-      }
-    }
-  }
-
   tags = {
     Name = "${var.project_name}-detector"
   }
+}
+
+resource "aws_guardduty_detector_feature" "s3_protection" {
+   detector_id = aws_guardduty_detector.main.id
+   name = "S3_DATA_EVENTS"
+   status      = "ENABLED"
+}
+
+resource "aws_guardduty_detector_feature" "eks_protection" {
+   detector_id = aws_guardduty_detector.main.id
+   name        = "EKS_AUDIT_LOGS"
+   status      = "ENABLED"
+}
+
+resource "aws_guardduty_detector_feature" "malware_protection" {
+   detector_id = aws_guardduty_detector.main.id
+   name        = "EBS_MALWARE_PROTECTION"
+   status      = "ENABLED"
 }
 
 # GuardDuty publishing destination (optional - to S3)
